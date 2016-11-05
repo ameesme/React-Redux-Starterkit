6f5ffda00731f237ee84c697c6b379917e1a2dc8
Mees Boeijen <mees@amees.me>
2016-11-05 14:48:34 +0100

    Implemented AVA unit-testing

--- src/js/components/app/app.spec.js
+++ src/js/components/app/app.spec.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import test from 'tape';
+import test from 'ava';
 import { shallow } from 'enzyme';
 import { App } from '.';
 
 test('<App />', t => {
-  t.test('Renders main div', st => {
     const wrapper = shallow(<App />);
-    st.equals(wrapper.find('div .main').length, 1, 'Found one main div');
-    st.end();
-  });
-
-  t.end();
+    t.is(wrapper.find('div .main').length, 1, 'Found one main div');
 });
