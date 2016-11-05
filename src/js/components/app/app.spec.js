e53c863b7cfdb3643a894306a62f8d3800d6a74c
Mees Boeijen <mees@amees.me>
2016-11-05 14:51:27 +0100

    Linting

--- src/js/components/app/app.spec.js
+++ src/js/components/app/app.spec.js
@@ -4,6 +4,6 @@ import { shallow } from 'enzyme';
 import { App } from '.';
 
 test('<App />', t => {
-    const wrapper = shallow(<App />);
-    t.is(wrapper.find('div .main').length, 1, 'Found one main div');
+  const wrapper = shallow(<App />);
+  t.is(wrapper.find('div .main').length, 1, 'Found one main div');
 });
